648e10c1e2fc87d7096bff4d90483ce1fa1f39b4
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2022-10-07 15:36:59 +0100

    feat: #14 Spring Messaging interceptors added

---
+++ lsd-interceptors/src/main/java/io/lsdconsulting/interceptors/messaging/EventPublisherInterceptor.java
@@ -0,0 +1,31 @@
+package io.lsdconsulting.interceptors.messaging;
+
+import com.lsd.LsdContext;
+import com.lsd.diagram.ValidComponentName;
+import lombok.RequiredArgsConstructor;
+import lsd.format.PrettyPrinter;
+import org.springframework.integration.config.GlobalChannelInterceptor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
+
+import static io.lsdconsulting.interceptors.http.common.Headers.HeaderKeys.SOURCE_NAME;
+import static io.lsdconsulting.interceptors.http.common.Headers.HeaderKeys.TARGET_NAME;
+
+@RequiredArgsConstructor
+@GlobalChannelInterceptor(patterns = "*-in-*", order = 100)
+public class EventPublisherInterceptor implements ChannelInterceptor {
+
+    private final LsdContext lsdContext;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        String payload = PrettyPrinter.prettyPrint(new String((byte[]) message.getPayload()));
+        String source = (String) message.getHeaders().get(SOURCE_NAME.key());
+        String target = (String) message.getHeaders().get(TARGET_NAME.key());
+
+        lsdContext.capture("Consuming event from " + ValidComponentName.of(source) + " to " + ValidComponentName.of(target), payload);
+
+        return message;
+    }
+}
---
+++ lsd-interceptors/src/test/java/io/lsdconsulting/interceptors/messaging/EventConsumerInterceptorTest.java
@@ -0,0 +1,43 @@
+package io.lsdconsulting.interceptors.messaging;
+
+import com.lsd.LsdContext;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+
+import java.util.Map;
+
+import static io.lsdconsulting.interceptors.http.common.Headers.HeaderKeys.SOURCE_NAME;
+import static io.lsdconsulting.interceptors.http.common.Headers.HeaderKeys.TARGET_NAME;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class EventConsumerInterceptorTest {
+    private final ArgumentCaptor<String> patternCaptor = ArgumentCaptor.forClass(String.class);
+    private final ArgumentCaptor<String> payloadCaptor = ArgumentCaptor.forClass(String.class);
+
+    private final LsdContext lsdContext = mock(LsdContext.class);
+    private final Message<byte[]> message = mock(Message.class);
+
+    private final EventConsumerInterceptor underTest = new EventConsumerInterceptor(lsdContext);
+
+    @Test
+    void logInteraction() {
+        given(message.getPayload()).willReturn("{\"key\":\"value\"}".getBytes(UTF_8));
+        given(message.getHeaders()).willReturn(new MessageHeaders(Map.of(SOURCE_NAME.key(), "Source", TARGET_NAME.key(), "Target")));
+
+        underTest.preSend(message, null);
+
+        verify(lsdContext).capture(patternCaptor.capture(), payloadCaptor.capture());
+        assertThat(patternCaptor.getValue()).isEqualTo("Consuming event from Source to Target");
+        assertThat(payloadCaptor.getValue()).isEqualTo(
+                "{\n" +
+                "  \"key\": \"value\"\n" +
+                "}"
+        );
+    }
+}
