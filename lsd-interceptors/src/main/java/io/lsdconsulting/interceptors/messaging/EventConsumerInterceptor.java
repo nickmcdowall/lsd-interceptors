cf62d0db957623cd5c09cd8332095e93f68376af
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-02-16 19:51:08 +0000

    feat: Handle more types of intercepted messages

--- lsd-interceptors/src/main/java/io/lsdconsulting/interceptors/messaging/EventConsumerInterceptor.java
+++ lsd-interceptors/src/main/java/io/lsdconsulting/interceptors/messaging/EventConsumerInterceptor.java
@@ -3,13 +3,13 @@ package io.lsdconsulting.interceptors.messaging;
 import com.lsd.LsdContext;
 import com.lsd.diagram.ValidComponentName;
 import lombok.RequiredArgsConstructor;
-import lsd.format.PrettyPrinter;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.ChannelInterceptor;
 
 import static io.lsdconsulting.interceptors.http.common.Headers.HeaderKeys.SOURCE_NAME;
 import static io.lsdconsulting.interceptors.http.common.Headers.HeaderKeys.TARGET_NAME;
+import static io.lsdconsulting.interceptors.messaging.PayloadRetriever.getPayload;
 
 @RequiredArgsConstructor
 public class EventConsumerInterceptor implements ChannelInterceptor {
@@ -18,7 +18,7 @@ public class EventConsumerInterceptor implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
-        String payload = PrettyPrinter.prettyPrint(new String((byte[]) message.getPayload()));
+        String payload = getPayload(message);
         String source = (String) message.getHeaders().get(SOURCE_NAME.key());
         String target = (String) message.getHeaders().get(TARGET_NAME.key());
 
