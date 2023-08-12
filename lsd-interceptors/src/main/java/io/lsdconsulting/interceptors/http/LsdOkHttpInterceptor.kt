f44675ce80a4f1d3ef0bd0810e9bf3aa0c0ac4ef
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-08-12 08:01:00 +0100

    fix: ok http converted to Kotlin

---
+++ lsd-interceptors/src/main/java/io/lsdconsulting/interceptors/http/LsdOkHttpInterceptor.kt
@@ -0,0 +1,64 @@
+package io.lsdconsulting.interceptors.http
+
+import io.lsdconsulting.interceptors.common.Headers
+import io.lsdconsulting.interceptors.http.common.HttpInteractionHandler
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import okio.Buffer
+import java.io.IOException
+import java.time.Duration
+import java.util.function.Consumer
+
+const val RESPONSE_MAXY_BYTES = 10000
+
+data class LsdOkHttpInterceptor(
+    private var handlers: List<HttpInteractionHandler>
+) : Interceptor {
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val requestCopy = request.newBuilder().build()
+        val path = request.url().encodedPath()
+        val requestHeaders = Headers.singleValueMap(request.headers().toMultimap())
+        handlers.forEach(Consumer { handler: HttpInteractionHandler ->
+            handler.handleRequest(
+                request.method(),
+                requestHeaders,
+                path,
+                bodyToString(requestCopy)
+            )
+        })
+        val start = System.currentTimeMillis()
+
+        val response = chain.proceed(request)
+
+        val duration = Duration.ofMillis(System.currentTimeMillis() - start)
+        val responseHeaders = Headers.singleValueMap(response.headers().toMultimap())
+        handlers.forEach(Consumer { handler: HttpInteractionHandler ->
+            handler.handleResponse(
+                response.code().toString() + " " + response.message(),
+                requestHeaders,
+                responseHeaders,
+                path,
+                copyBodyString(response),
+                duration
+            )
+        })
+        return response
+    }
+
+    /*
+     * Prevent closing the response body stream by peeking. The max bytes is to prevent OOM for ridiculous size bodies
+     */
+    private fun copyBodyString(response: Response): String {
+        return response.peekBody(RESPONSE_MAXY_BYTES.toLong()).string()
+    }
+
+    private fun bodyToString(copy: Request): String {
+        val buffer = Buffer()
+        copy.body()!!.writeTo(buffer)
+        return buffer.readUtf8()
+    }
+}
