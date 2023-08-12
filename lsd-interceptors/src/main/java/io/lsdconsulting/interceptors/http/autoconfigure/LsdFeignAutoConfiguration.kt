9f2f175a3052cb9e0a95bcdac0ba02d750ccbb1c
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-08-12 09:30:37 +0100

    fix: autoconfigs converted to Kotlin

---
+++ lsd-interceptors/src/main/java/io/lsdconsulting/interceptors/http/autoconfigure/LsdFeignAutoConfiguration.kt
@@ -0,0 +1,51 @@
+package io.lsdconsulting.interceptors.http.autoconfigure
+
+import com.lsd.core.LsdContext
+import feign.Logger.Level
+import io.lsdconsulting.interceptors.http.LsdFeignLoggerInterceptor
+import io.lsdconsulting.interceptors.http.common.HttpInteractionHandler
+import lombok.RequiredArgsConstructor
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cloud.openfeign.FeignClientBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.PropertySource
+
+/**
+ *
+ *
+ * If a [LsdContext] class is available it will automatically autoconfig a [LsdFeignLoggerInterceptor]
+ *
+ * <br></br>
+ *
+ *
+ * By default *source* name defaults to *'App'* and the *destination* name will be derived using a
+ * [io.lsdconsulting.interceptors.http.naming.RegexResolvingNameMapper].
+ *
+ * <br></br>
+ *
+ *
+ * Users can override either or both of the default name mappings by supplying their own [io.lsdconsulting.interceptors.http.naming.SourceNameMappings] or
+ * [io.lsdconsulting.interceptors.http.naming.DestinationNameMappings] beans and naming them *'defaultSourceNameMapping`* and *'defaultDestinationNameMapping`*.
+ *
+ */
+@Configuration
+@PropertySource(ignoreResourceNotFound = true, value = ["classpath:lsd.properties"])
+@ConditionalOnProperty(name = ["lsd.interceptors.autoconfig.enabled"], havingValue = "true", matchIfMissing = true)
+@ConditionalOnClass(value = [LsdContext::class, FeignClientBuilder::class, Level::class])
+@Import(NamingConfig::class, HttpHandlerConfig::class)
+@RequiredArgsConstructor
+open class LsdFeignAutoConfiguration(
+    private val httpInteractionHandlers: List<HttpInteractionHandler>
+) {
+
+    @Bean
+    open fun lsdFeignLoggerInterceptor() = LsdFeignLoggerInterceptor(httpInteractionHandlers)
+
+    @Bean
+    @ConditionalOnMissingBean
+    open fun feignLoggerLevel(): Level = Level.BASIC
+}
