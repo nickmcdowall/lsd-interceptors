171bb8c40b6aa673a6c0ed6e3ff939279f2a5939
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-09-01 20:45:00 +0100

    fix: Update lsd-core and remove PLANTUML special character handling

--- lsd-interceptors/src/main/kotlin/io/lsdconsulting/interceptors/rabbitmq/EventNameDeriver.kt
+++ lsd-interceptors/src/main/kotlin/io/lsdconsulting/interceptors/rabbitmq/EventNameDeriver.kt
@@ -2,18 +2,15 @@ package io.lsdconsulting.interceptors.rabbitmq
 
 import org.springframework.amqp.core.MessageProperties
 
-fun deriveEventName(messageProperties: MessageProperties, alternativeExchangeName: String?): String =
-    getDefaultExchangeName(alternativeExchangeName).let { defaultExchangeName ->
-        if (!(messageProperties.getHeader(TYPE_ID_HEADER) as String?).isNullOrBlank()) {
-            deriveFromTypeIdHeader(messageProperties.getHeader(TYPE_ID_HEADER))
-        } else defaultExchangeName
-    }
+fun deriveEventName(messageProperties: MessageProperties, alternativeEventName: String?): String =
+    deriveFromTypeIdHeader(messageProperties.getHeader(TYPE_ID_HEADER))
+        ?: getDefaultExchangeName(alternativeEventName)
 
 private fun getDefaultExchangeName(alternativeExchangeName: String?): String =
     if (!alternativeExchangeName.isNullOrBlank()) alternativeExchangeName else UNKNOWN_EVENT
 
-private fun deriveFromTypeIdHeader(typeIdHeader: String): String =
-    typeIdHeader.split("\\.".toRegex()).reduce { _: String?, second: String -> second }
+private fun deriveFromTypeIdHeader(typeIdHeader: String?): String? =
+    typeIdHeader?.split("\\.".toRegex())?.reduce { _: String?, second: String -> second }?.ifBlank { null }
 
 private const val TYPE_ID_HEADER = "__TypeId__"
 private const val UNKNOWN_EVENT = "UNKNOWN_EVENT"
