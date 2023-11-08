9d446eda9cf234fe1477d98d770495cfc36d2ee4
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-11-08 09:34:06 +0000

    fix: update java version and springframework libs
    BREAKING CHANGE: builds with java 17 and updates spring versions

--- lsd-interceptors/src/test/kotlin/io/lsdconsulting/interceptors/http/StubHttpRequest.kt
+++ lsd-interceptors/src/test/kotlin/io/lsdconsulting/interceptors/http/StubHttpRequest.kt
@@ -1,6 +1,7 @@
 package io.lsdconsulting.interceptors.http
 
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpRequest
 import java.net.URI
 
@@ -8,18 +9,8 @@ data class StubHttpRequest(
     private val methodValue: String,
     private val uri: URI,
     private val httpHeaders: HttpHeaders
-) :
-    HttpRequest {
-
-    override fun getHeaders(): HttpHeaders {
-        return httpHeaders
-    }
-
-    override fun getMethodValue(): String {
-        return methodValue
-    }
-
-    override fun getURI(): URI {
-        return uri
-    }
+) : HttpRequest {
+    override fun getHeaders(): HttpHeaders = httpHeaders
+    override fun getMethod(): HttpMethod = HttpMethod.valueOf(methodValue)
+    override fun getURI(): URI = uri
 }
